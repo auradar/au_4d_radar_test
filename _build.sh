62fd2af3bb57f92877ab3a2e1c41195f5f2d927a
hyunilko <hyunilko@gmail.com>
2026-03-19 17:16:23 +0900

    update _build.sh

--- _build.sh
+++ _build.sh
@@ -5,10 +5,9 @@ echo "== build only =="
 else
 echo "== remove and build =="
 rm -rf install build log
-rm -rf install/au_4d_radar/ build/ log/
 
-export AMENT_PREFIX_PATH=$(echo "$AMENT_PREFIX_PATH" | tr ':' '\n' | grep -v "install/au_4d_radar" | paste -sd:)
-export CMAKE_PREFIX_PATH=$(echo "$CMAKE_PREFIX_PATH" | tr ':' '\n' | grep -v "install/au_4d_radar" | paste -sd:)
+export AMENT_PREFIX_PATH=$(echo "$AMENT_PREFIX_PATH" | tr ':' '\n' | grep -v "au_4d_radar_test/install" | paste -sd:)
+export CMAKE_PREFIX_PATH=$(echo "$CMAKE_PREFIX_PATH" | tr ':' '\n' | grep -v "au_4d_radar_test/install" | paste -sd:)
 fi
 
 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release --packages-select au_4d_radar radar_msgs mon_msgs tf_publisher_radar
